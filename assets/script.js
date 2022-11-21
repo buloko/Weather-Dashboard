89cf91dd70326e4905226566a30ecd0d0ebc97dc
buloko <buloko8@gmail.com>
2022-11-20 21:43:47 -0500

    commit format

--- assets/script.js
+++ assets/script.js
@@ -1,26 +1,26 @@
 //Global Variables
 var searchHistory[];
-var weatherAPIRootURL= `"http://api.openweathermap.org`;
-var APIKey ="d3d868c125c76948db80ecf5668f6693";
-var inputEL= document.querySelector("#city-input");
+var weatherAPIRootURL = `"http://api.openweathermap.org`;
+var APIKey = "d3d868c125c76948db80ecf5668f6693";
+var inputEL = document.querySelector("#city-input");
 var searchBtn = document.querySelector("#search-btn")
 // function that gets us the specific cities weather information
 function apiFetch(city) {
-var url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`
-fetch(url)
-.then((response)=>{
-    return response.json()
-}).then((data)=>{
-    console.log(data);
+    var url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`
+    fetch(url)
+        .then((response) => {
+            return response.json()
+        }).then((data) => {
+            console.log(data);
 
 
-})
+        })
 }
 
 
-searchBtn.addEventListener("click",(e)=>{
+searchBtn.addEventListener("click", (e) => {
     e.preventDefault()
-    var userInput=inputEL.value
+    var userInput = inputEL.value
     console.log(userInput);
     apiFetch(userInput)
 })
